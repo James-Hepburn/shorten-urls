609e44329c8a944e3b7863e59625fbf3417b110a
James-Hepburn <90709675+James-Hepburn@users.noreply.github.com>
2025-03-15 10:57:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var url = "https://api.shorten-jeh.com/shorten";
+
+document.querySelector ("form").addEventListener ("submit", async function (event) {
+    event.preventDefault ();
+
+    var long = document.getElementById ("url").value;
+    var link = document.getElementById ("link");
+
+    try {
+        var response = await fetch (url, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify ({long_url: long}),
+        });
+
+        if (!response.ok) throw new Error ("Failed to shorten URL.");
+
+        var raw = await response.json ();
+        var data = raw.body ? JSON.parse (raw.body) : raw;
+
+        if (data.short_url) {
+            link.innerHTML = `
+                Shortened URL: <a href="${data.short_url}" target="_blank">${data.short_url}</a>
+            `;
+        } else {
+            throw new Error("Shortened URL not found in response.");
+        }
+    } catch (error) {
+        link.textContent = "Error: Could not shorten the URL.";
+    }
+});
